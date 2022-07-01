6e466aae5aa146946103ab6bc2e7c93417c807ab
kwonz <kwonzhu@gmail.com>
2022-07-01 18:36:20 +0930

    change SignUpModal to class component, add state for input

--- app/PageHeader/components/SignUpModal/SignUpModal.jsx
+++ app/PageHeader/components/SignUpModal/SignUpModal.jsx
@@ -17,28 +17,59 @@ const SignUpButton = styled(Button)`
   width: 100%;
 `;
 
-const SignUpModal = ({
-  closeModal,
-  onSignUp, //(user) => this.handleUserChange(user)
-}) => (
-  <Modal onClose={closeModal}>
-    <Title>Join Us</Title>
-    <CloseButton onClick={closeModal} />
-    <Form>
-      <FormItem label="Email" htmlFor="sign-up-modal-email">
-        <Input id="sign-up-modal-email" />
-      </FormItem>
-      <FormItem label="Password" htmlFor="sign-up-modal-password">
-        <Input type="password" id="sign-up-modal-password" />
-      </FormItem>
-      <FormItem label="Confirm password" htmlFor="sign-up-modal-confirm-password">
-        <Input type="password" id="sign-up-modal-confirm-password" />
-      </FormItem>
-    </Form>
-    <SignUpButton size="md" variant="success">
-      Join Airtasker
-    </SignUpButton>
-  </Modal>
-);
+class SignUpModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+  }
+
+  handleEmailChange(newEmail) {
+    this.setState({ 
+      email: newEmail,
+    }); 
+  }
+
+  render() {
+    const { closeModal } = this.props;
+    const { email, password, confirmPassword } = this.state;
+    return (
+      <Modal onClose={closeModal}>
+        <Title>Join Us</Title>
+        <CloseButton onClick={closeModal} />
+        <Form
+          onSubmit={(event) => {
+            event.preventDefault();
+          
+          console.log(email);
+        }}
+        >
+          <FormItem label="Email" htmlFor="sign-up-modal-email">
+            <Input 
+              value={email}
+              onChange={(event) => this.handleEmailChange(event.target.value)}
+              id="sign-up-modal-email" 
+            />
+          </FormItem>
+          <FormItem label="Password" htmlFor="sign-up-modal-password">
+            <Input type="password" id="sign-up-modal-password" />
+          </FormItem>
+          <FormItem label="Confirm password" htmlFor="sign-up-modal-confirm-password">
+            <Input type="password" id="sign-up-modal-confirm-password" />
+          </FormItem>
+        </Form>
+        <SignUpButton size="md" variant="success">
+          Join Airtasker
+        </SignUpButton>
+      </Modal>
+    )
+  }
+}
 
 export default SignUpModal;

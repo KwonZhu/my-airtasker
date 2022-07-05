6e9ac30d1191a6518c3e2bcce949a46f9723660e
kwonz <kwonzhu@gmail.com>
2022-07-05 23:02:30 +0930

    ...prevState in SignUpModal

--- app/PageHeader/components/SignUpModal/SignUpModal.jsx
+++ app/PageHeader/components/SignUpModal/SignUpModal.jsx
@@ -22,16 +22,23 @@ class SignUpModal extends React.Component {
     super(props);
 
     this.state = {
-      data: null,
+      data: {
+        email: '',
+        password: '',
+        confirmPassword: '',
+      }
     }
 
     this.handleDataChange = this.handleDataChange.bind(this);
   }
 
-  handleDataChange(newData) {
-    this.setState({ 
-      data: newData,
-    }); 
+  handleDataChange(key, value) { //update the value corresponding to key
+    this.setState((prevState) => ({ 
+      data: {
+        ...prevState.data,
+        [key]: value,
+      },
+    })); 
   }
 
   render() {
@@ -45,26 +52,32 @@ class SignUpModal extends React.Component {
           onSubmit={(event) => {
             event.preventDefault();
 
-            console.log(data);
+            console.log('state', this.state);
           }}
         >
           <FormItem label="Email" htmlFor="sign-up-modal-email">
             <Input 
-              value={data} //initial value
-              onChange={(event) => this.handleDataChange(event.target.value)} //Triggered when value change
+              value={data.email} //initial value
+              onChange={(event) => this.handleDataChange('email', event.target.value)} //Triggered when value change
               id="sign-up-modal-email" 
             />
           </FormItem>
           <FormItem label="Password" htmlFor="sign-up-modal-password">
-            <Input type="password" id="sign-up-modal-password" />
+            <Input 
+              value={data.password}
+              onChange={(event) => this.handleDataChange('password', event.target.value)}
+              type="password" id="sign-up-modal-password" />
           </FormItem>
           <FormItem label="Confirm password" htmlFor="sign-up-modal-confirm-password">
-            <Input type="password" id="sign-up-modal-confirm-password" />
+            <Input 
+              value={data.confirmPassword}
+              onChange={(event) => this.handleDataChange('confirmPassword', event.target.value)}
+              type="password" id="sign-up-modal-confirm-password" />
           </FormItem>
+          <SignUpButton size="md" variant="success">
+            Join Airtasker
+          </SignUpButton>
         </Form>
-        <SignUpButton size="md" variant="success">
-          Join Airtasker
-        </SignUpButton>
       </Modal>
     )
   }
